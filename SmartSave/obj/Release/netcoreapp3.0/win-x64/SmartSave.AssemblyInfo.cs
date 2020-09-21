c42c0a82b56cb8e5053c7b048f946c7fc5d3a5d9
Caroline Chirenje <carochire@gmail.com>
2020-09-21 18:40:23 +0200

    Active Tab after redirect

--- SmartSave/obj/Release/netcoreapp3.0/win-x64/SmartSave.AssemblyInfo.cs
+++ SmartSave/obj/Release/netcoreapp3.0/win-x64/SmartSave.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("9330986b-5e12-4bd0-bdf6-36fbf1a35c18")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartSave")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartSave")]
